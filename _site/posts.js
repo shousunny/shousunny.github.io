469f0132823fb08ad7d6d5266dbeb29aa8bcc0bc
shousunny <shousunny@foxmail.com>
2018-01-14 10:07:28 +0800

    copy

--- _site/posts.js
+++ _site/posts.js
@@ -6,6 +6,26 @@ $(document).ready(function() {
 	    "title":"shousunny的第一篇博客",
         "url":"http://localhost:4000/blog/2018/01/11/Jan",
         "date":"11 Jan 2018"
+    },
+{
+	    "title":"四面楚歌",
+        "url":"http://localhost:4000/blog/2012/10/09/surrounded-by-enemies-on-all-sides",
+        "date":"09 Oct 2012"
+    },
+{
+	    "title":"九月：来京九年记",
+        "url":"http://localhost:4000/blog/2012/09/24/september",
+        "date":"24 Sep 2012"
+    },
+{
+	    "title":"欲写已忘言",
+        "url":"http://localhost:4000/blog/2012/03/20/forgot-what-to-say",
+        "date":"20 Mar 2012"
+    },
+{
+	    "title":"《闪电狗》",
+        "url":"http://localhost:4000/blog/2012/01/22/bolt",
+        "date":"22 Jan 2012"
     }
 
 
